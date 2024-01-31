1998bf8b9bdc71f56516f866778b31542ee14b92
mahen-alim <mahennekkers27@gmail.com>
2024-01-31 08:38:33 +0700

    add: upload file project laravel

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <div>
+        <h1>ArenaFinder</h1>
+        <h3>Hierarki</h3>
+        <h3>Bantuan</h3>
+        <h3>Narahubung</h3>
+        <h3>Subscription</h3>
+        <h3>Sosial Media</h3>
+        <h5>Workspace</h5>
+    </div>
+</div>
